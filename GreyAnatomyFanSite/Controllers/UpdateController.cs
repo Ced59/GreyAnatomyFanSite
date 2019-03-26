62daebd5ca9e215faca8e833ea7ebed294c3d689
Cedric Caudron <c.caudron59@gmail.com>
2019-03-26 16:49:34 +0100

    AddUpdates

---
+++ GreyAnatomyFanSite/Controllers/UpdateController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreyAnatomyFanSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreyAnatomyFanSite.Controllers
+{
+    public class UpdateController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.NbreVisitUnique = GetVisitIP();
+            UserConnect(ViewBag);
+
+            return View();
+        }
+
+
+        private void UserConnect(dynamic v)
+        {
+            bool? logged = Convert.ToBoolean(HttpContext.Session.GetString("logged"));
+            if (logged == true)
+            {
+                v.Logged = logged;
+                v.Pseudo = HttpContext.Session.GetString("pseudo");
+                v.Id = HttpContext.Session.GetString("idMembre");
+                v.Avatar = HttpContext.Session.GetString("avatar");
+                v.Statut = HttpContext.Session.GetString("statut");
+
+
+                if (HttpContext.Session.GetString("statut") == "Coeur")
+                {
+                    v.MessageBonjour = "mon Coeur";
+                }
+                else
+                {
+                    v.MessageBonjour = HttpContext.Session.GetString("pseudo");
+                }
+
+            }
+
+            else
+            {
+                v.Logged = false;
+            }
+        }
+
+        private int GetVisitIP()
+        {
+            string remoteIpAddress = Convert.ToString(Request.HttpContext.Connection.RemoteIpAddress);
+            Visiteur v = new Visiteur(remoteIpAddress);
+            return v.GetVisit(v);
+        }
+    }
+}
