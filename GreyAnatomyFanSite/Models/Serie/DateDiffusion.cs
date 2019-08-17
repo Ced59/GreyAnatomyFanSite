8d0d91ba63a38c60dbe79c6e720dd84f98aca1ca
Cedric Caudron <c.caudron59@gmail.com>
2019-08-17 11:14:38 +0200

    Add Objects Scenarists Realisator

---
+++ GreyAnatomyFanSite/Models/Serie/DateDiffusion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreyAnatomyFanSite.Models.Serie
+{
+    public class DateDiffusion
+    {
+        private int idEpisode;
+        
+
+        public int IdEpisode { get => idEpisode; set => idEpisode = value; }
+        
+    }
+}
--- GreyAnatomyFanSite/Models/Serie/Episode.cs
+++ GreyAnatomyFanSite/Models/Serie/Episode.cs
@@ -9,24 +9,28 @@ namespace GreyAnatomyFanSite.Models.Serie
     public class Episode
     {
         private string titre;
+        private string titreFr;
         private string synopsis;
         private List<Personnage> personnages;
-        private DateTime premiereDiffusion;
-        private DateTime premiereDiffusionFrance;
         private List<Patient> patients;
         private List<string> photos;
         private int idEpisode;
         private int idSaison;
+        private Realisateur realisateur;
+        private Scenarist scenarist;
+        private int noProduction;
 
 
         public string Titre { get => titre; set => titre = value; }
         public string Synopsis { get => synopsis; set => synopsis = value; }
         public List<Personnage> Personnages { get => personnages; set => personnages = value; }
-        public DateTime PremiereDiffusion { get => premiereDiffusion; set => premiereDiffusion = value; }
-        public DateTime PremiereDiffusionFrance { get => premiereDiffusionFrance; set => premiereDiffusionFrance = value; }
         public List<Patient> Patients { get => patients; set => patients = value; }
         public List<string> Photos { get => photos; set => photos = value; }
         public int IdEpisode { get => idEpisode; set => idEpisode = value; }
         public int IdSaison { get => idSaison; set => idSaison = value; }
+        public string TitreFr { get => titreFr; set => titreFr = value; }
+        public Realisateur Realisateur { get => realisateur; set => realisateur = value; }
+        public Scenarist Scenarist { get => scenarist; set => scenarist = value; }
+        public int NoProduction { get => noProduction; set => noProduction = value; }
     }
 }
