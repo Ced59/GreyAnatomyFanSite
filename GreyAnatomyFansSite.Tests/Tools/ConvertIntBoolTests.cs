16e3bc9b27de4528a217b04dd65f12e686e01596
Cedric <c.caudron59@gmail.com>
2020-09-21 09:38:28 +0200

    Adding test static class ConvertIntBool

---
+++ GreyAnatomyFansSite.Tests/Tools/ConvertIntBoolTests.cs
@@ -0,0 +1,66 @@
+﻿using GreyAnatomyFanSite.Tools;
+using System;
+using Xunit;
+
+namespace GreyAnatomyFansSite.Tests.Tools
+{
+    public class ConvertIntBoolTests
+    {
+        [Fact]
+        public void GivenBoolTrue_WhenCallConvertBoolToInt_ThenReturn1()
+        {
+            //Arrange
+            var entry = true;
+
+            //Act
+            var actualValue = ConvertIntBool.ConvertBoolToInt(entry);
+            var expectedValue = 1;
+
+            //Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GivenBoolFalse_WhenCallConvertBoolToInt_ThenReturn0()
+        {
+            //Arrange
+            var entry = false;
+
+            //Act
+            var actualValue = ConvertIntBool.ConvertBoolToInt(entry);
+            var expectedValue = 0;
+
+            //Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GivenBool0_WhenCallConvertIntToBool_ThenReturnFalse()
+        {
+            //Arrange
+            var entry = 0;
+
+            //Act
+            var actualValue = ConvertIntBool.ConvertIntToBool(entry);
+            var expectedValue = false;
+
+            //Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GivenBool1_WhenCallConvertIntToBool_ThenReturnTrue()
+        {
+            //Arrange
+            var entry = 1;
+
+            //Act
+            var actualValue = ConvertIntBool.ConvertIntToBool(entry);
+            var expectedValue = true;
+
+            //Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+    }
+}
